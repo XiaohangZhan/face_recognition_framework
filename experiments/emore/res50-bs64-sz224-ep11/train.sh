a3fddf9de18dc6fbd45b20304b9fcbeac2dc0070
xiaohangzhan <xiaohangzhan@outlook.com>
2018-10-13 21:35:56 +0800

    fix ngpu

--- experiments/emore/res50-bs64-sz224-ep11/train.sh
+++ experiments/emore/res50-bs64-sz224-ep11/train.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 work_path=$(dirname $0)
-GLOG_vmodule=MemcachedClient=-1 srun --mpi=pmi2 -p Spring_face -n1 --gres=gpu:8 --ntasks-per-node=1 \
+GLOG_vmodule=MemcachedClient=-1 srun --mpi=pmi2 -p Spring_1080 -n1 --gres=gpu:8 --ntasks-per-node=1 \
     python -u main.py \
-    --config $work_path/config.yaml \
-    --ngpu 8
+    --config $work_path/config.yaml
