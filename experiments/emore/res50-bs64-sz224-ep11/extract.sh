db5e43337e85658273948194f28a34e2c1798d0b
xiaohangzhan <xiaohangzhan@outlook.com>
2019-08-01 22:18:02 +0800

    update

--- experiments/emore/res50-bs64-sz224-ep11/extract.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-work_path=$(dirname $0)
-    python -u main.py \
-    --config $work_path/config.yaml \
-    --load-path $work_path/checkpoints/ckpt_epoch_10.pth.tar \
-    --extract
--- experiments/emore/res50arc-bs64-sz224-ep11/train.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-work_path=$(dirname $0)
-    python -u main.py \
-    --config $work_path/config.yaml
